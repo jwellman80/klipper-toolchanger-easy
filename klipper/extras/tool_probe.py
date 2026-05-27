ae0cfcae0367bd6847a4205cf52230a874049e81
N3MI-DG <129717180+N3MI-DG@users.noreply.github.com>
2026-05-27 18:50:14 +1000

    Bring probe code up to latest Klipper standards.
    Breaks backward compatibility.

--- klipper/extras/tool_probe.py
+++ klipper/extras/tool_probe.py
@@ -5,31 +5,167 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging
 from . import probe
+from . import manual_probe
 
 class ToolProbe:
     def __init__(self, config):
+        self.tool = config.getint('tool')
         self.printer = config.get_printer()
-        self.tool_number = config.getint('tool', None)
         self.name = config.get_name()
-
-        pin = config.get('pin')
-        ppins = self.printer.lookup_object('pins')
-        ppins.allow_multi_use_pin(pin.replace('^', '').replace('!', ''))
-
-        self.mcu_probe = probe.ProbeEndstopWrapper(config)
         self.probe_offsets = probe.ProbeOffsetsHelper(config)
         self.param_helper = probe.ProbeParameterHelper(config)
+        self.mcu_probe = probe.ProbeEndstopWrapper(config, self.probe_offsets,
+                                                   self.param_helper)
+        self.probe_session = ProbeSessionHelper(config, self.mcu_probe, self.probe_offsets)
 
-        if self.tool_number is not None:
-            # Crash detection stuff
-            buttons = self.printer.load_object(config, 'buttons')
-            buttons.register_buttons([pin], self._button_handler)
-            #Register with the endstop
-            self.endstop = self.printer.load_object(config, "tool_probe_endstop")
-            self.endstop.add_probe(config, self)
+        # Crash detection stuff
+        pin = config.get('pin')
+        buttons = self.printer.load_object(config, 'buttons')
+        ppins = self.printer.lookup_object('pins')
+        ppins.allow_multi_use_pin(pin.replace('^', '').replace('!', ''))
+        buttons.register_buttons([pin], self._button_handler)
+
+        #Register with the endstop
+        self.endstop = self.printer.load_object(config, "tool_probe_endstop")
+        self.endstop.add_probe(config, self)
 
     def _button_handler(self, eventtime, is_triggered):
         self.endstop.note_probe_triggered(self, eventtime, is_triggered)
+
+    def get_probe_params(self, gcmd=None):
+        return self.probe_session.get_probe_params(gcmd)
+    def get_offsets(self, gcmd=None):
+        return self.probe_offsets.get_offsets(gcmd)
+    def start_probe_session(self, gcmd):
+        return self.probe_session.start_probe_session(gcmd)
+
+# Helper to track multiple probe attempts in a single command
+class ProbeSessionHelper:
+    def __init__(self, config, mcu_probe, probe_offsets):
+        self.printer = config.get_printer()
+        self.mcu_probe = mcu_probe
+        self.probe_offsets = probe_offsets
+        gcode = self.printer.lookup_object('gcode')
+        self.dummy_gcode_cmd = gcode.create_gcode_command("", "", {})
+        # Configurable probing speeds
+        self.speed = config.getfloat('speed', 5.0, above=0.)
+        self.lift_speed = config.getfloat('lift_speed', self.speed, above=0.)
+        # Multi-sample support (for improved accuracy)
+        self.sample_count = config.getint('samples', 1, minval=1)
+        self.sample_retract_dist = config.getfloat('sample_retract_dist', 2.,
+                                                   above=0.)
+        atypes = {'median': 'median', 'average': 'average'}
+        self.samples_result = config.getchoice('samples_result', atypes,
+                                               'average')
+        self.samples_tolerance = config.getfloat('samples_tolerance', 0.100,
+                                                 minval=0.)
+        self.samples_retries = config.getint('samples_tolerance_retries', 0,
+                                             minval=0)
+        # Session state
+        self.multi_probe_pending = False
+        self.hw_session = None
+        self.results = []
+        # Register event handlers
+        self.printer.register_event_handler("gcode:command_error",
+                                            self._handle_command_error)
+    def _handle_command_error(self):
+        if self.multi_probe_pending:
+            try:
+                self.end_probe_session()
+            except:
+                logging.exception("Multi-probe end")
+    def _probe_state_error(self):
+        raise self.printer.command_error(
+            "Internal probe error - start/end probe session mismatch")
+    def start_probe_session(self, gcmd):
+        if self.multi_probe_pending:
+            self._probe_state_error()
+        self.hw_session = self.mcu_probe.start_probe_session(gcmd)
+        self.multi_probe_pending = True
+        self.results = []
+        return self
+    def end_probe_session(self):
+        if not self.multi_probe_pending:
+            self._probe_state_error()
+        self.results = []
+        self.multi_probe_pending = False
+        self.hw_session.end_probe_session()
+        self.hw_session = None
+    def get_probe_params(self, gcmd=None):
+        if gcmd is None:
+            gcmd = self.dummy_gcode_cmd
+        probe_speed = gcmd.get_float("PROBE_SPEED", self.speed, above=0.)
+        lift_speed = gcmd.get_float("LIFT_SPEED", self.lift_speed, above=0.)
+        samples = gcmd.get_int("SAMPLES", self.sample_count, minval=1)
+        sample_retract_dist = gcmd.get_float("SAMPLE_RETRACT_DIST",
+                                             self.sample_retract_dist, above=0.)
+        samples_tolerance = gcmd.get_float("SAMPLES_TOLERANCE",
+                                           self.samples_tolerance, minval=0.)
+        samples_retries = gcmd.get_int("SAMPLES_TOLERANCE_RETRIES",
+                                       self.samples_retries, minval=0)
+        samples_result = gcmd.get("SAMPLES_RESULT", self.samples_result)
+        return {'probe_speed': probe_speed,
+                'lift_speed': lift_speed,
+                'samples': samples,
+                'sample_retract_dist': sample_retract_dist,
+                'samples_tolerance': samples_tolerance,
+                'samples_tolerance_retries': samples_retries,
+                'samples_result': samples_result}
+    def _probe(self, gcmd):
+        toolhead = self.printer.lookup_object('toolhead')
+        curtime = self.printer.get_reactor().monotonic()
+        if 'z' not in toolhead.get_status(curtime)['homed_axes']:
+            raise self.printer.command_error("Must home before probe")
+        try:
+            self.hw_session.run_probe(gcmd)
+            epos = self.hw_session.pull_probed_results()[0]
+        except self.printer.command_error as e:
+            reason = str(e)
+            if "Timeout during endstop homing" in reason:
+                reason += probe.HINT_TIMEOUT
+            raise self.printer.command_error(reason)
+        # Allow axis_twist_compensation to update results
+        results = [epos]
+        self.printer.send_event("probe:update_results", results)
+        epos = results[0]
+        # Report results
+        gcode = self.printer.lookup_object('gcode')
+        gcode.respond_info("probe at %.3f,%.3f is z=%.6f"
+                           % (epos.bed_x, epos.bed_y, epos.bed_z))
+        return epos
+    def run_probe(self, gcmd):
+        if not self.multi_probe_pending:
+            self._probe_state_error()
+        params = self.get_probe_params(gcmd)
+        toolhead = self.printer.lookup_object('toolhead')
+        probexy = toolhead.get_position()[:2]
+        retries = 0
+        positions = []
+        sample_count = params['samples']
+        while len(positions) < sample_count:
+            # Probe position
+            pos = self._probe(gcmd)
+            positions.append(pos)
+            # Check samples tolerance
+            z_positions = [p.bed_z for p in positions]
+            if max(z_positions)-min(z_positions) > params['samples_tolerance']:
+                if retries >= params['samples_tolerance_retries']:
+                    raise gcmd.error("Probe samples exceed samples_tolerance")
+                gcmd.respond_info("Probe samples exceed tolerance. Retrying...")
+                retries += 1
+                positions = []
+            # Retract
+            if len(positions) < sample_count:
+                toolhead.manual_move(
+                    probexy + [pos.bed_z + params['sample_retract_dist']],
+                    params['lift_speed'])
+        # Calculate result
+        epos = probe.calc_probe_z_average(positions, params['samples_result'])
+        self.results.append(epos)
+    def pull_probed_results(self):
+        res = self.results
+        self.results = []
+        return res
 
 def load_config_prefix(config):
     return ToolProbe(config)
--- klipper/extras/tool_probe_endstop.py
+++ klipper/extras/tool_probe_endstop.py
@@ -4,17 +4,32 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 from . import probe
+from . import manual_probe
+
+# Helper class to provide probe offsets interface for ToolProbeEndstop
+class ToolProbeOffsetsHelper:
+    def __init__(self, tool_probe_endstop):
+        self.tool_probe_endstop = tool_probe_endstop
+
+    def get_offsets(self, gcmd=None):
+        return self.tool_probe_endstop.get_offsets(gcmd)
+
+    # Support legacy Klipper versions where HomingViaProbeHelper calls this
+    def create_probe_result(self, test_pos):
+        x_offset, y_offset, z_offset = self.tool_probe_endstop.get_offsets()
+        return manual_probe.ProbeResult(
+            test_pos[0]+x_offset, test_pos[1]+y_offset,
+            test_pos[2]-z_offset, test_pos[0], test_pos[1], test_pos[2])
 
 # Virtual endstop, using a tool attached Z probe in a toolchanger setup.
 # Tool endstop change may be done either via SET_ACTIVE_TOOL_PROBE TOOL=99
 # Or via auto-detection of single open tool probe via DETECT_ACTIVE_TOOL_PROBE
 class ToolProbeEndstop:
-    def __init__(self, config, standalone = True):
+    def __init__(self, config):
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
         self.name = config.get_name()
-        self.probes = []
-        self.tool_number_to_probe = {}
+        self.tool_probes = {}
         self.last_query = {} # map from tool number to endstop state
         self.active_probe = None
         self.active_tool_number = -1
@@ -22,14 +37,10 @@ class ToolProbeEndstop:
         self.crash_detection_active = False
         self.crash_lasttime = 0.
         self.mcu_probe = EndstopRouter(self.printer)
-        self.probe = ProbeRouter(self.printer)
-        self.probe_offsets = self.probe
-        self.param_helper = self.probe
+        self.probe_offsets = ToolProbeOffsetsHelper(self)
+        probe.ProbeParameterHelper(config)  # consume speed/samples config options
+        probe.HomingViaProbeHelper(config, 0., self.mcu_probe.query_endstop)
         self.cmd_helper = probe.ProbeCommandHelper(config, self, self.mcu_probe.query_endstop)
-        self.homing_helper = probe.HomingViaProbeHelper(
-            config, self.mcu_probe, self.probe_offsets, self.param_helper)
-        self.probe_session = probe.ProbeSessionHelper(
-            config, self.param_helper, self.homing_helper.start_probe_session)
 
         # Emulate the probe object, since others rely on this.
         if self.printer.lookup_object('probe', default=None):
@@ -42,43 +53,38 @@ class ToolProbeEndstop:
                                             self._handle_connect)
         # Register PROBE/QUERY_PROBE commands
         self.gcode = self.printer.lookup_object('gcode')
-        if standalone:
-            self.gcode.register_command('SET_ACTIVE_TOOL_PROBE', self.cmd_SET_ACTIVE_TOOL_PROBE,
-                                        desc=self.cmd_SET_ACTIVE_TOOL_PROBE_help)
-            self.gcode.register_command('DETECT_ACTIVE_TOOL_PROBE', self.cmd_DETECT_ACTIVE_TOOL_PROBE,
-                                        desc=self.cmd_DETECT_ACTIVE_TOOL_PROBE_help)
-            self.gcode.register_command('START_TOOL_PROBE_CRASH_DETECTION', self.cmd_START_TOOL_PROBE_CRASH_DETECTION,
-                                        desc=self.cmd_START_TOOL_PROBE_CRASH_DETECTION_help)
-            self.gcode.register_command('STOP_TOOL_PROBE_CRASH_DETECTION', self.cmd_STOP_TOOL_PROBE_CRASH_DETECTION,
-                                        desc=self.cmd_STOP_TOOL_PROBE_CRASH_DETECTION_help)
-
-    def get_probe_params(self, gcmd=None):
-        return self.param_helper.get_probe_params(gcmd)
-    def get_offsets(self, gcmd=None):
-        return self.probe_offsets.get_offsets(gcmd)
-    def start_probe_session(self, gcmd):
-        return self.probe_session.start_probe_session(gcmd)
-    def get_status(self, eventtime):
-        status = self.cmd_helper.get_status(eventtime)
-        offsets = self.get_offsets()
-        status['last_tools_query'] = self.last_query
-        status['active_tool_number'] = self.active_tool_number
-        status['active_tool_probe'] = self.active_probe.name if self.active_probe else None
-        status['active_tool_probe_x_offset'] = offsets[0]
-        status['active_tool_probe_y_offset'] = offsets[1]
-        status['active_tool_probe_z_offset'] = offsets[2]
-        return status
+        self.gcode.register_command('SET_ACTIVE_TOOL_PROBE', self.cmd_SET_ACTIVE_TOOL_PROBE,
+                                    desc=self.cmd_SET_ACTIVE_TOOL_PROBE_help)
+        self.gcode.register_command('DETECT_ACTIVE_TOOL_PROBE', self.cmd_DETECT_ACTIVE_TOOL_PROBE,
+                                    desc=self.cmd_DETECT_ACTIVE_TOOL_PROBE_help)
+        self.gcode.register_command('START_TOOL_PROBE_CRASH_DETECTION', self.cmd_START_TOOL_PROBE_CRASH_DETECTION,
+                                    desc=self.cmd_START_TOOL_PROBE_CRASH_DETECTION_help)
+        self.gcode.register_command('STOP_TOOL_PROBE_CRASH_DETECTION', self.cmd_STOP_TOOL_PROBE_CRASH_DETECTION,
+                                    desc=self.cmd_STOP_TOOL_PROBE_CRASH_DETECTION_help)
 
     def _handle_connect(self):
         self.toolhead = self.printer.lookup_object('toolhead')
         self._detect_active_tool()
 
+    def get_offsets(self, gcmd=None):
+        if self.active_probe:
+            return self.active_probe.get_offsets(gcmd)
+        return 0.0, 0.0, 0.0
+    
+    def get_probe_params(self, gcmd=None):
+        if self.active_probe:
+            return self.active_probe.get_probe_params(gcmd)
+        raise self.printer.command_error("No active tool probe")
+    
+    def start_probe_session(self, gcmd):
+        if self.active_probe:
+            return self.active_probe.start_probe_session(gcmd)
+        raise self.printer.command_error("No active tool probe")
+
     def add_probe(self, config, tool_probe):
-        if tool_probe.tool_number is not None:
-            if tool_probe.tool_number in self.tool_number_to_probe:
-                raise config.error(f"Duplicate tool probe nr: {tool_probe.tool_number}")
-            self.tool_number_to_probe[tool_probe.tool_number] = tool_probe
-        self.probes.append(tool_probe)
+        if (tool_probe.tool in self.tool_probes):
+            raise config.error("Duplicate tool probe nr: %s" % (tool_probe.tool,))
+        self.tool_probes[tool_probe.tool] = tool_probe
         self.mcu_probe.add_mcu(tool_probe.mcu_probe)
 
     def set_active_probe(self, tool_probe):
@@ -86,11 +92,9 @@ class ToolProbeEndstop:
             return
         self.active_probe = tool_probe
         if self.active_probe:
-            self.probe.set_active_probe(tool_probe)
             self.mcu_probe.set_active_mcu(tool_probe.mcu_probe)
-            self.active_tool_number = self.active_probe.tool_number
+            self.active_tool_number = self.active_probe.tool
         else:
-            self.probe.set_active_probe(None)
             self.mcu_probe.set_active_mcu(None)
             self.active_tool_number = -1
 
@@ -98,10 +102,9 @@ class ToolProbeEndstop:
         print_time = self.toolhead.get_last_move_time()
         self.last_query.clear()
         candidates = []
-        for tool_probe in self.probes:
+        for tool_probe in self.tool_probes.values():
             triggered = tool_probe.mcu_probe.query_endstop(print_time)
-            if tool_probe.tool_number is not None:
-                self.last_query[tool_probe.tool_number] = triggered
+            self.last_query[tool_probe.tool] = triggered
             if not triggered:
                 candidates.append(tool_probe)
         return candidates
@@ -130,9 +133,9 @@ class ToolProbeEndstop:
     cmd_SET_ACTIVE_TOOL_PROBE_help = "Set the tool probe that will act as the Z endstop."
     def cmd_SET_ACTIVE_TOOL_PROBE(self, gcmd):
         probe_nr = gcmd.get_int("T")
-        if probe_nr not in self.tool_number_to_probe:
+        if (probe_nr not in self.tool_probes):
             raise gcmd.error("SET_ACTIVE_TOOL_PROBE no tool probe for tool %d" % (probe_nr))
-        self.set_active_probe(self.tool_number_to_probe[probe_nr])
+        self.set_active_probe(self.tool_probes[probe_nr])
 
     cmd_DETECT_ACTIVE_TOOL_PROBE_help = "Detect which tool is active by identifying a probe that is NOT triggered"
     def cmd_DETECT_ACTIVE_TOOL_PROBE(self, gcmd):
@@ -144,6 +147,22 @@ class ToolProbeEndstop:
         else:
             self.set_active_probe(None)
             gcmd.respond_info(self._describe_tool_detection_issue(active_tools))
+
+    def get_status(self, eventtime):
+        status = self.cmd_helper.get_status(eventtime)
+        status['last_tools_query'] = self.last_query
+        status['active_tool_number'] = self.active_tool_number
+        if self.active_probe:
+            status['active_tool_probe'] = self.active_probe.name
+            status['active_tool_probe_x_offset'] = self.active_probe.get_offsets()[0]
+            status['active_tool_probe_y_offset'] = self.active_probe.get_offsets()[1]
+            status['active_tool_probe_z_offset'] = self.active_probe.get_offsets()[2]
+        else:
+            status['active_tool_probe'] = None
+            status['active_tool_probe_x_offset'] = 0.0
+            status['active_tool_probe_y_offset'] = 0.0
+            status['active_tool_probe_z_offset'] = 0.0
+        return status
 
     cmd_START_TOOL_PROBE_CRASH_DETECTION_help = "Start detecting tool crashes"
     def cmd_START_TOOL_PROBE_CRASH_DETECTION(self, gcmd):
@@ -187,29 +206,6 @@ class ToolProbeEndstop:
             self.crash_detection_active = False
             self.crash_gcode.run_gcode_from_command()
 
-class ProbeRouter:
-    def __init__(self, printer):
-        self.printer = printer
-        self.active_probe = None
-
-    def set_active_probe(self, probe):
-        self.active_probe = probe
-
-    # Offsets helper
-    def get_offsets(self, *args, **kwargs):
-        if not self.active_probe:
-            return  0.0, 0.0, 0.0
-        return self.active_probe.probe_offsets.get_offsets(*args, **kwargs)
-    def create_probe_result(self, *args, **kwargs):
-        if not self.active_probe:
-            raise self.printer.command_error("Cannot query endstop - no active tool probe.")
-        return self.active_probe.probe_offsets.create_probe_result(*args, **kwargs)
-    # Param helper
-    def get_probe_params(self, *args, **kwargs):
-        if not self.active_probe:
-            raise self.printer.command_error("Cannot query endstop - no active tool probe.")
-        return self.active_probe.param_helper.get_probe_params(*args, **kwargs)
-
 # Routes commands to the selected tool probe endstop.
 class EndstopRouter:
     def __init__(self, printer):
@@ -221,33 +217,12 @@ class EndstopRouter:
 
     def add_mcu(self, mcu_probe):
         self._mcus.append(mcu_probe)
-        for s in self._steppers:
-            mcu_probe.add_stepper(s)
 
     def set_active_mcu(self, mcu_probe):
         self.active_mcu = mcu_probe
-        # Update Wrappers
-        if self.active_mcu:
-            self.get_mcu = self.active_mcu.get_mcu
-            self.home_start = self.active_mcu.home_start
-            self.home_wait = self.active_mcu.home_wait
-            self.multi_probe_begin = self.active_mcu.multi_probe_begin
-            self.multi_probe_end = self.active_mcu.multi_probe_end
-            self.probe_prepare = self.active_mcu.probe_prepare
-            self.probe_finish = self.active_mcu.probe_finish
-        else:
-            self.get_mcu = self.on_error
-            self.home_start = self.on_error
-            self.home_wait = self.on_error
-            self.multi_probe_begin = self.on_error
-            self.multi_probe_end = self.on_error
-            self.probe_prepare = self.on_error
-            self.probe_finish = self.on_error
 
     def add_stepper(self, stepper):
         self._steppers.append(stepper)
-        for m in self._mcus:
-            m.add_stepper(stepper)
     def get_steppers(self):
         return list(self._steppers)
 
@@ -258,11 +233,7 @@ class EndstopRouter:
             raise self.printer.command_error("Cannot query endstop - no active tool probe.")
         return self.active_mcu.query_endstop(print_time)
     def get_position_endstop(self):
-        if not self.active_mcu:
-            # This will get picked up by the endstop, and is static
-            # Report 0 and fix up in the homing sequence
-            return 0.0
-        return self.active_mcu.get_position_endstop()
+        return 0.0
 
 def load_config(config):
     return ToolProbeEndstop(config)
