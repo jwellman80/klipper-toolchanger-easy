d74539f24680d81202cc3701fd244ceeeffa252b
Riccardo Merli <40023353+rmerli@users.noreply.github.com>
2026-08-23 14:17:10 +0200

    Update manual_rail.py
    Klipper's core PrinterHoming.manual_home() (klippy/extras/homing.py) added a new
    required positional argument, probe_pos, inserted between speed and triggered.
    Current upstream Klipper signature:    
    def manual_home(self, toolhead, endstops, pos, speed,
                        probe_pos, triggered, check_triggered):

--- klipper/extras/manual_rail.py
+++ klipper/extras/manual_rail.py
@@ -93,13 +93,13 @@ class ManualRail:
         self.do_set_position(forcepos)
         endstops = self.rail.get_endstops()
         phoming = self.printer.lookup_object('homing')
-        phoming.manual_home(self, endstops, pos, hi.speed, True, True)
+        phoming.manual_home(self, endstops, pos, hi.speed, False, True, True)
         # Perform second home
         if hi.retract_dist:
             retract_dist = -hi.retract_dist if hi.positive_dir else hi.retract_dist
             self.do_move(hi.position_endstop + retract_dist, hi.speed, accel)
             self.do_set_position(hi.position_endstop + retract_dist * 1.5)
-            phoming.manual_home(self, endstops, pos, hi.second_homing_speed, True, True)
+            phoming.manual_home(self, endstops, pos, hi.second_homing_speed, False, True, True)
     cmd_MANUAL_RAIL_help = "Command a manually configured rail"
     def cmd_MANUAL_RAIL(self, gcmd):
         if gcmd.get('GCODE_AXIS', None) is not None:
